611a4847735043dd664e7103d47f81acc7ca2467
Elizabeth <elizabeth.por@gmail.com>
2022-04-29 14:36:31 -0500

    added authorization on backend then commented out

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,14 @@
 class IngredientsController < ApplicationController
+# before_action :authorize
+
     def index
         render json: Ingredient.all
     end
+
+    private
+
+    # def authorize
+    #     return render json: {error: "You are not authorized to view this content, please log in"}, status: :unauthorized unless session.include? :user_id
+    # end
+
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,14 @@
 class RecipesController < ApplicationController
+    # before_action :authorize 
+
     def index
         render json: Recipe.all
     end
+
+    private
+
+    # def authorize
+    #     return render json: {error: "You are not authorized to view this content, please log in"}, status: :unauthorized unless session.include? :user_id
+    # end
+
 end
