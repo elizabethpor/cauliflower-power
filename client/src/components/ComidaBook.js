6a2378caa6891d512c7cd5cdda41d57f308d4db7
Elizabeth <elizabeth.por@gmail.com>
2022-05-03 17:30:44 -0500

    changed text content

--- client/src/components/ComidaBook.js
+++ client/src/components/ComidaBook.js
@@ -18,9 +18,20 @@ function ComidaBook({user, setUser}) {
 
                 <h1>Comida</h1>
                 <h3>- a recipe book -</h3>
-                <p>I wrote this book to inspire people to make wholesole, nutricious, delicious food on a budget. It contains 28 original recipes to easily design your own 4-week-cycle menu. In an effort to reduce food waste, many ingredients are repeated in the recipes but cooked/used uin different ways. The recipes pay attention to allergies: they are all vegan (with options to add non-vegan ingredients), nut free, and most of them are gluten free. This book was inspired by my career in the foodservice industry where meals - called Comida - served to staff are usually none of the above. Recipes are designed to be scalable, starting with 2 people or 10 people. I hope you'll enjoy making and eating this food as much as I do!</p>
+                <p>I spent many happy years working in the food service industry; as barista, busser, server, bartender, manager, and chef.
+When restaurants serve food to their staff during a shift, it’s called family meal, or often, simply “Comida.” 
+From the point of view of staff, Comida is an opportunity for their work-family to come together and enjoy food. It’s also an opportunity for a restaurant to show their most important asset that they are valued. Unfortunately, all too often, Comida is not delicious, nutritions, or wholesome. 
+
+When Comida is unhealthy and discourteous of food restrictions, a kind gesture turns into a point of frustration for staff. I wrote this book to make a simple path to better Comida. It contains 28 original recipes that are delicious, nutritions, or wholesome. 
+The recipes are inexpensive, ingredient-flexible (86 food waste!) and mindful of a wide range of food preferences. Most recipes are gluten-free and all are nut free and vegan (with options to add non-vegan ingredients on the side).
+Recipes are designed to be easily scalable in quantity and they preserve well if made ahead and refrigerated.
+Whether you work in a restaurant or want to use this manual at home, I hope you enjoy making and serving this food to all the families in your life!
+<br></br>
+<br></br>
+-Elizabeth</p>
+<br></br>
                 <ComidaPhotoGallery />
-                <Button as={Link} to="/checkout" exact>Purchase the book </Button>
+                <Button as={Link} to="/checkout" exact>Purchase the e-book</Button>
             </Grid>
         </Grid>
         </div>
