2ef01ab863ab634fdbe0fcc71385f4b8172e5236
Elizabeth <elizabeth.por@gmail.com>
2022-05-11 17:52:10 -0500

    code works on frontend and backend. still have to render favorites on page

--- client/src/components/RecipeDetail.js
+++ client/src/components/RecipeDetail.js
@@ -2,22 +2,23 @@ import React, {useState, useEffect} from 'react';
 import Grid from '@mui/material/Grid';
 import RDNavBar from "./RDNavBar";
 import { Link, useParams } from "react-router-dom";
+import { Button } from "../styles";
 
 function RecipeDetail({user, setUser}) {
     const [recipe, setRecipe] = useState()
+    const [isFavorite, setIsFavorite] = useState(false)
 
   function LoadItem(id) {
     fetch(`/recipes/${id}`)
       .then(r => r.json())
       .then(recipe => {
-          console.log(recipe)
+        console.log(recipe)
         setRecipe(recipe);
       })
   }
 
   const data = useParams();
-  // console.log("params:",data)
-  // console.log("windows",window.location.pathname)
+
   useEffect(() => {
     LoadItem(data.id);
   }, [data]);
@@ -25,6 +26,30 @@ function RecipeDetail({user, setUser}) {
   if (!recipe) return null
 
   const ingredients = recipe.ingredient_list.map((ingredient) => <li>{ingredient}</li>);
+
+  function handleUnfavor() {
+    setIsFavorite(false)
+    //delete
+    fetch(`/remove-fav-recipe/${recipe.id}`, {
+      method: "DELETE",
+  })
+      .then((r) => r.json())
+      .then((unfavoredRecipe) => console.log(unfavoredRecipe));
+  };
+
+  function handleFavor() {
+    setIsFavorite(true)
+    // post
+    fetch(`/add-fav-recipe/${recipe.id}`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recipe.id)
+  })
+      .then((r) => r.json())
+      .then(data => console.log(data));
+  };
 
     return (
         <div>
@@ -35,12 +60,19 @@ function RecipeDetail({user, setUser}) {
             <Grid item xs={10} md={10}>
                 <h1>{recipe.name}</h1>
                 <h3>{recipe.allergy_code}</h3>
+                {isFavorite ? 
+              <Button variant="fill" onClick={handleUnfavor}>Saved as favorite</Button>
+              :
+              <Button variant="outline" onClick={handleFavor}>Save as favorite</Button>
+              }
                 <h2>Ingredients list:</h2>
                 <ul>
                     {ingredients}
                 </ul>
                 <h2>Instructions</h2>
                 <p>{recipe.instructions}</p>
+
+                {/* <Button onClick={handleClick}>{isFavorite ? "Save as favorite" : "Saved as favorite"}</Button> */}
             </Grid>
             </Grid>
         </div>
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_04_200008) do
+ActiveRecord::Schema.define(version: 2022_05_11_185141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_favorites_on_recipe_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -47,6 +56,8 @@ ActiveRecord::Schema.define(version: 2022_05_04_200008) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favorites", "recipes"
+  add_foreign_key "favorites", "users"
   add_foreign_key "ingredients_recipes", "ingredients"
   add_foreign_key "ingredients_recipes", "recipes"
 end
