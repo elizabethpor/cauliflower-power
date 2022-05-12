7c18793e4ceee009c95b66e494cdff86d20c9076
Elizabeth <elizabeth.por@gmail.com>
2022-05-12 14:25:36 -0500

    updated recipes for 2 ppl

--- client/src/components/RecipeResults.js
+++ client/src/components/RecipeResults.js
@@ -4,6 +4,7 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import Recipe from "./Recipe";
 import RRNavBar from "./RRNavBar";
+import styled from "styled-components";
 
 function RecipeResults({user, setUser, recipes, setSelectedIngredients}) {
 
@@ -14,15 +15,21 @@ function RecipeResults({user, setUser, recipes, setSelectedIngredients}) {
                 <RRNavBar  user={user} setUser={setUser} setSelectedIngredients={setSelectedIngredients}/>
             </Grid>
             <Grid item xs={10} md={10}>
+                
             <ImageList sx={{ width: 1000, height: 400 }} cols={4} gap={8}>
                     {recipes.map((recipe) => (
                         <Recipe key={recipe.id} recipe={recipe} />
                     ))}
                 </ImageList>
+                <H3>all recipes are designed to be for 2 people</H3>
             </Grid>
             </Grid>
         </div>
     )
 };
+
+const H3 = styled.h3`
+    text-align: center;
+`;
 
 export default RecipeResults;
