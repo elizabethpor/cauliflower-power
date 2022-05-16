9b619762806c1080ab6f1a0e635c73874dbb6f7a
Elizabeth <elizabeth.por@gmail.com>
2022-05-16 09:24:06 -0500

    routed stripe response to home

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -12,8 +12,8 @@ class ChargesController < ApplicationController
         mode: 'payment',
         # success_url: 'http://localhost:4000/success' + '?success=true',
         # cancel_url: 'http://localhost:4000/canceled' + '?canceled=true',
-        success_url: 'https://cauliflower-power.herokuapp.com/success' + '?success=true',
-        cancel_url: 'https://cauliflower-power.herokuapp.com/canceled' + '?canceled=true',
+        success_url: 'https://cauliflower-power.herokuapp.com/' + '?success=true',
+        cancel_url: 'https://cauliflower-power.herokuapp.com/' + '?canceled=true',
       })
       redirect_to session.url, status: 303
     end
